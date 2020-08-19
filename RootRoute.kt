ff9a40ae37023be3a885bcfb0a6bc15127b2a9f8
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2020-08-19 16:18:51 +0200

    updates

--- RootRoute.kt
+++ RootRoute.kt
@@ -1,18 +1,33 @@
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Routing.rootSignal() {
-    get("/signal") {
-        call.respondText("Signal", ContentType.Text.Plain)
+    install(StatusPages) {
+        exception<Throwable> { e ->
+            call.respondText(e.localizedMessage, ContentType.Text.Plain, HttpStatusCode.InternalServerError)
+        }
+    }
+    install(ContentNegotiation) {
+        //moshi()
+    }
+
+    post("/signal") {
+        val parameters = call.receiveParameters()
+
+        val paramValue1 = parameters["id"]
+        val paramValue2 = parameters["timestamp"]
+
+        call.respondText("Received signal $paramValue1 and $paramValue2\n")
     }
 }
 
 fun Routing.rootReceive() {
     get("/receive") {
-        call.respondText("Receive", ContentType.Text.Plain)
+        call.respondText("This is a GET route")
     }
 
 }
